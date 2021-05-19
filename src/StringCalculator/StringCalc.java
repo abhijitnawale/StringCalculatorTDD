2ad2254bd9227992e6b00643e09c145b27f7a789
abhijit <abhijitnawale1112@gmail.com>
2021-05-19 11:35:06 +0530

    testEmptyString

---
+++ src/StringCalculator/StringCalc.java
@@ -0,0 +1,12 @@
+package StringCalculator;
+
+public class StringCalc {
+
+	public static long add1(String input) {
+		// TODO Auto-generated method stub
+		if (input.equals("")) {
+			return 0;
+		}
+        return 0;
+	}
+}
---
+++ src/TestStringCalculator/StringCalcTest.java
@@ -0,0 +1,18 @@
+package TestStringCalculator;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import StringCalculator.StringCalc;
+
+class StringCalcTest {
+
+	
+	@Test
+	public void testEmptyString() {
+		assertEquals(0, StringCalc.add1(" 1"));
+	}
+
+}
